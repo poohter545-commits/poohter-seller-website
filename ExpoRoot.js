a1b10fd7e6bbb1cc753c365435e84e9dc95d2a65
Poohter <poohter545@gmail.com>
2026-05-24 21:52:07 +0500

    Bust root seller iframe cache

--- ExpoRoot.js
+++ ExpoRoot.js
@@ -2,7 +2,7 @@ import { StyleSheet, View } from "react-native";
 
 const SellerDashboardFrame = () => (
   <iframe
-    src="/seller/index.html"
+    src="/seller/index.html?v=20260524-product-click-order"
     title="Poohter Seller Dashboard"
     style={styles.frame}
   />
